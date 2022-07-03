d70acc3a69aeee88b5e3d3dddbc256129dd0ae3e
fredericktky <fredericktky@gmail.com>
2022-07-03 17:19:33 +0800

    update

--- main.java
+++ main.java
@@ -3,19 +3,20 @@ import java.util.ArrayList; //array
 
 
 public class main {
-    public static void main(String[] args) {
+    public main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double totalcost = 0; //variable
-        double gst = 3;
+        double totalCost=0;
+        double GST = 3;
         int x = 0;
-        Boolean promo= false;
-        String promocode= "P10";
-        Double promoDiscount= 10.0;
 
-        ArrayList<String> Itemslist = new ArrayList<>(); //array
-        ArrayList<Double> ItemsPrice = new ArrayList<>();
-        ArrayList<Integer> ItemsQuantity = new ArrayList<>();
+        boolean promo;
+        String promoCode= "P10";
+        double promoDiscount= 10.0;
+
+        ArrayList<String> itemsList = new ArrayList<>();
+        ArrayList<Double> itemsPrice = new ArrayList<>();
+        ArrayList<Integer> itemsQuantity = new ArrayList<>();
 
         System.out.println("Welcome to Shopping Cart Calculator"); //Welcome
 
@@ -25,24 +26,24 @@ public class main {
                 if (Newitem.equals("y")){
                     System.out.println("Input name"); //Get input of itemname, add to array
                     String itemname = scanner.next();
-                    Itemslist.add(itemname);
+                    itemsList.add(itemname);
 
                     System.out.println("Input price"); //Get input of itemprice, add to array
-                    Double price = scanner.nextDouble();
-                    ItemsPrice.add(price);
+                    double price = scanner.nextDouble();
+                    itemsPrice.add(price);
 
                     System.out.println("Quantity"); //Get input of item quantity, add to array
                     int quantity = scanner.nextInt();
-                    ItemsQuantity.add(quantity);
+                    itemsQuantity.add(quantity);
 
-                    Double totalItemPrice= price*quantity; //Calculate total item price
-                    totalcost= totalcost+totalItemPrice;
+                    double totalItemPrice= price*quantity; //Calculate total item price
+                    totalCost= totalCost+totalItemPrice;
                     System.out.println (itemname +":"+ totalItemPrice);}
 
                 else if (Newitem.equals("n")){
                     System.out.println("Promo code:");
                     String inputPromocode= scanner.next();
-                        if (inputPromocode.equals(promocode)){
+                        if (inputPromocode.equals(promoCode)){
                             promo=true;
                             System.out.println("Promo code valid");
                         }
@@ -51,21 +52,21 @@ public class main {
                         }
 
                     System.out.println("SHOPPING CART SUMMARY"+
-                            "\nItem bought:" +Itemslist+
-                            "\nItem price:RM"+ItemsPrice+
-                            "\nItem Quantity:"+ItemsQuantity);
+                            "\nItem bought:" +itemsList+
+                            "\nItem price:RM"+itemsPrice+
+                            "\nItem Quantity:"+itemsQuantity);
                         if(promo) {
-                            Double priceDeducted = totalcost * promoDiscount / 100;
+                            double priceDeducted = totalCost * promoDiscount / 100;
                             System.out.println("Discount: RM" + priceDeducted);
                         }
                         else{
                             System.out.println("No Discount");
                     }
-                    gst= totalcost*gst/100; //Calculate total cart price and exit
-                    System.out.println("GST: RM"+gst);
-                    totalcost=totalcost+gst;
-                    System.out.println("Total cart: RM"+totalcost);
-                    x=+1;}
+                    double gstCost= totalCost*GST/100; //Calculate total cart price and exit
+                    System.out.println("GST: RM"+gstCost);
+                    totalCost=totalCost+gstCost;
+                    System.out.println("Total cart: RM"+totalCost);
+                    x=1;}
                 else{
                     System.out.println("Please enter valid selection");}
 
